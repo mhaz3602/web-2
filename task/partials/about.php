31caf8a70c5d3feb3d1db7b77c0fe128c50c1a02
mhaz3602 <mhaz36002@gmail.com>
2025-03-12 21:18:53 +0700

    mengumpulkan tugas pertemuan 3

---
+++ task/partials/about.php
@@ -0,0 +1,20 @@
+<section class="bg-light py-5">
+    <div class="container px-5">
+        <div class="row gx-5 justify-content-center">
+            <div class="col-xxl-8">
+                <div class="text-center my-5">
+                    <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
+                    <p class="lead fw-light mb-4">My name is Muhammad Hudzaifah ALi Al-Mubarok and I help them grow.</p>
+                    <p class="text-muted">Kegagalan adalah pelajaran, keberhasilan sebuah perjalanan. Teruslah belajar, teruslah tumbuh.🌱✨
+                    </p>
+                    <div class="d-flex justify-content-center fs-2 gap-4">
+                        <a class="text-gradient" href="https://www.instagram.com/mhaz326/"><i class="bi bi-twitter"></i></a>
+                        <a class="text-gradient" href="https://www.linkedin.com/in/m-hudzaifah-ali-al-mubarok-a1a83132b/"><i class="bi bi-linkedin"></i></a>
+                        <a class="text-gradient" href="https://github.com/mhaz3602"><i class="bi bi-github"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
